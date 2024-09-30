928eadcfe79f1996b7e6ad7c74d778a963ec475a
daphneforever <daphnemwilkerson@gmail.com>
2024-09-29 22:19:08 -0700

    create third logo animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,9 @@ const root_styles = getComputedStyle(root);
 const css_white = root_styles.getPropertyValue('--white').trim();
 const css_contrast = root_styles.getPropertyValue('--dark-contrast').trim();
 const css_red = root_styles.getPropertyValue('--red').trim();
-// var rand_rotation = root.styles.getPropertyValue('--random-rotation');
-// var rand_y = root.styles.getPropertyValue('--random-translation-y');
+const slice_map = new Map([["a-slice", "#slice-1"], ["r-1-slice", "#slice-2"], ["m-1-slice", "#slice-3"], 
+    ["m-2-slice", "#slice-4"], ["m-3-slice", "#slice-5"], ["o-slice", "#slice-6"], ["r-2-slice", "#slice-7"], ["y-slice", "#slice-8"]]);
+let slice_set = new Set();
 
 $(function()
 {
@@ -54,4 +55,20 @@ $("#logo-2-bounds").on("mouseenter", function()
 {
     $(this).find("#knife").removeClass("throw").addClass("return");
     $(this).find(".logo-flags").find("path").css({"transform": "translateX(0px)"});
+});
+
+$("#logo-3-bounds div").on("mouseenter", function()
+{   
+    let slice = $(slice_map.get(this.id));
+    slice.removeClass("slice-return").addClass("slice-fall");
+    slice_set.add(slice);
+});
+
+$("#logo-3-bounds").on("mouseleave", function()
+{
+    slice_set.forEach(el => {
+        el.removeClass("slice-fall").addClass("slice-return");
+    });
+    // $(this).find("#bottom-segment path").removeClass("slice-fall").addClass("slice-return");
+    slice_set.clear();
 });
