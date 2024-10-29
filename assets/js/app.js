a407a9bc9c381ca1167e8d9d5bfca84f8eb72ad8
daphneforever <daphnemwilkerson@gmail.com>
2024-10-28 19:48:32 -0700

    implement contact page

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ const css_white = root_styles.getPropertyValue('--white');
 const css_contrast = root_styles.getPropertyValue('--dark-contrast');
 const css_red = root_styles.getPropertyValue('--red');
 const css_lightGreen = root_styles.getPropertyValue('--light-green');
+const css_lightestRed = root_styles.getPropertyValue('--lightest-red');
 
 const slice_map = new Map([["a-slice", "#slice-1"], ["r-1-slice", "#slice-2"], ["m-1-slice", "#slice-3"], 
     ["m-2-slice", "#slice-4"], ["m-3-slice", "#slice-5"], ["o-slice", "#slice-6"], ["r-2-slice", "#slice-7"], ["y-slice", "#slice-8"]]);
@@ -14,8 +15,10 @@ let selected_tab = 0;
 
 $(function()
 {
-    setTimeout(newTabSelected(), 100);
-    // startAutoScroll();
+    if (this.location.pathname === "/index.html")
+    {
+        setTimeout(newTabSelected(), 100);
+    }
 });
 
 $("#logo-1-bounds").on("mouseenter", function()
@@ -114,6 +117,11 @@ function newTabSelected()
         }
 
         contentSection.css({"opacity": 1});
+
+        if (selected_tab === 0)
+        {
+
+        }
     }, "500");
 };
 
@@ -255,19 +263,20 @@ function resetFeatures()
     wires.eq(2).attr({"d": "M50 735C50 735 50 506 50 506"});
     $(".feature-node").each(function(i) { $(this).attr({"cx": "50", "stroke": css_lightGreen}); });
     // $("#feature-box").css({"width": "0px"});
-    $("#feature-box").removeClass("box-open");
-    $("#feature-box").removeClass("box-close");
+    $(".feature-box").find("video").trigger("stop");
     $(".feature-text").each(function(i) { $(this).css({"left": "-75px"}); });
 }
 
 $("#content-section").on("mouseenter", ".feature-text", function()
 {
     resetFeatures();
+    $(".feature-box").removeClass("box-open");
+    $(".feature-box").removeClass("box-close");
 
     const index = $(".feature-text").index($(this));
     let wires = $(".feature-wire");
     let nodes = $(".feature-node");
-    let box = $("#feature-box");
+    let box = $(".feature-box").eq(index);
 
     $(this).css({"left": "0px"});
     // box.removeClass("box-close");
@@ -277,9 +286,9 @@ $("#content-section").on("mouseenter", ".feature-text", function()
     {
         wires.eq(0).attr({"d": "M50 278C50 50 180 50 180 50"});
         nodes.eq(0).attr({"cx": "180", "stroke": css_red});
-        box.load("../../assets/subhtml/lfofeature.html");
+        // box.load("../../assets/subhtml/lfofeature.html");
         // box.css({"width": "400px", "top": top});
-        box.css({"height": "200px"});
+        // box.css({"height": "200px"});
         
     }
     else if (index === 1)
@@ -287,38 +296,46 @@ $("#content-section").on("mouseenter", ".feature-text", function()
         wires.eq(0).attr({"d": "M180 278C180 278 50 278 50 50"});
         wires.eq(1).attr({"d": "M50 506C50 278 180 278 180 278"});
         nodes.eq(1).attr({"cx": "180", "stroke": css_red});
-        box.load("../../assets/subhtml/reorderingfeature.html");
+        // box.load("../../assets/subhtml/reorderingfeature.html");
         // box.css({"width": "400px", "top": top});
-        box.css({"height": "250px"});
+        // box.css({"height": "250px"});
     }
     else if (index === 2)
     {
         wires.eq(1).attr({"d": "M180 506C180 506 50 506 50 278"});
         wires.eq(2).attr({"d": "M50 735C50 508.5 180 506 180 506"});
         nodes.eq(2).attr({"cx": "180", "stroke": css_red});
-        box.load("../../assets/subhtml/envelopefeature.html");
+        // box.load("../../assets/subhtml/envelopefeature.html");
         // box.css({"width": "400px", "top": top});
-        box.css({"height": "200px"});
+        // box.css({"height": "200px"});
     }
     else if (index === 3)
     {
         wires.eq(2).attr({"d": "M180 735C180 735 50 735 50 506"});
         nodes.eq(3).attr({"cx": "180", "stroke": css_red});
-        box.load("../../assets/subhtml/uifeature.html");
+        // box.load("../../assets/subhtml/uifeature.html");
         // box.css({"width": "400px", "top": top});
-        box.css({"height": "250px"});
+        // box.css({"height": "250px"});
     }
 
-    const top = $(this).position().top - parseInt($("#feature-box").css("height")) / 2 + parseInt($(this).css("height")) / 2;
+    const top = $(this).position().top - parseInt(box.css("height")) / 2 + parseInt($(this).css("height")) / 2;
     box.css({"top": top});
+    box.find("video").trigger("load");
+    box.find("video").trigger("play");
     box.addClass("box-open");
 
 }).on("mouseleave", "#feature-container", function()
 {
     resetFeatures();
-    let box = $("#feature-box");
+    // const index = $(".feature-text").index($(this));
+    $(".feature-box").each(function(i) { 
+        if ($(this).hasClass("box-open")) { 
+            $(this).removeClass("box-open");
+            $(this).addClass("box-close"); 
+        } 
+    });
     // box.css({"width": "400px"});
-    box.addClass("box-close");
+    // box.addClass("box-close");
 });
 
 $("#content-section").on("mousedown", "#big-red-button", function()
@@ -330,3 +347,74 @@ $("#content-section").on("mousedown", "#big-red-button", function()
     $(this).attr({"src": "../../assets/images/big red button standby.svg"});
     window.location.href = "../../launchcodes-download.html";
 });
+
+$("#content-section").on("mouseenter", ".contact-item", function()
+{
+    $(this).find(".contact-box").removeClass("box-close");
+    $(this).find(".contact-box").addClass("box-open");
+
+}).on("mouseleave", ".contact-item", function()
+{
+    $(this).find(".contact-box").removeClass("box-open");
+    $(this).find(".contact-box").addClass("box-close");
+});
+
+$("#content-section").on("mouseenter", ".link-button", function()
+{
+    $(this).css({"background-color": css_lightestRed});
+
+}).on("mouseleave", ".link-button", function()
+{
+    $(this).css({"background-color": css_white});
+
+}).on("mousedown", ".link-button", function()
+{
+    $(this).css({"color": css_white});
+
+}).on("mouseup", ".link-button", function()
+{
+    $(this).css({"color": css_contrast});
+});
+
+$(".link-button").on("mouseenter", function()
+{
+    $(this).css({"background-color": css_lightestRed});
+
+}).on("mouseleave", function()
+{
+    $(this).css({"background-color": css_white});
+
+}).on("mousedown", function()
+{
+    $(this).css({"color": css_white});
+
+}).on("mouseup", function()
+{
+    $(this).css({"color": css_contrast});
+});
+
+function copyEmail() {
+    navigator.clipboard.writeText("daphnemwilkerson@gmail.com");
+}
+
+$("#content-section").on("mouseup", "#email-link", function()
+{
+    copyEmail();
+
+    let currentWidth = $(this).css("width");
+    $(this).html("<p>Copied!</p>");
+    $(this).css({"width": currentWidth});
+    setTimeout(() =>
+    {
+        $(this).html("<p>Copy email to clipboard</p>");
+    }, 2000);
+});
+
+$("#secondary-nav-bar").on("mouseenter", function()
+{
+    $(this).find("svg").css({"fill": css_red});
+
+}).on("mouseleave", function()
+{
+    $(this).find("svg").css({"fill": css_white});
+})
